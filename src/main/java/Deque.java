0b3e72e1ee4f1b8b810e1de6fe1cff5c5c76133a
Oukey <oukey311@gmail.com>
2021-01-13 20:20:39 +0400

    Add Deque

---
+++ src/main/java/Deque.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Deque<T> {
+    public Deque() {
+        // Конструктор внутреннего хранилища
+    }
+
+    public void addFront(T item) {
+        // Метод добавления в голову
+    }
+
+    public void addTail(T item) {
+        // Метод добавления в хвост
+    }
+
+    public T removeFront() {
+        // Метод удаления из головы
+        return null;
+    }
+
+    public T removeTail() {
+        // Метод удаления из хвоста
+        return null;
+    }
+
+    public int size() {
+        // размер очереди
+        return 0;
+    }
+}
--- src/main/java/Queue.java
+++ src/main/java/Queue.java
@@ -43,5 +43,5 @@ public class Queue<T> {
 //            return queue;
 //        }
 //    }
-
+// 40650
 }
