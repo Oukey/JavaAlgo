a5ace0a57addb3fd1ab7e381794a0a3fa54bb705
Oukey <oukey311@gmail.com>
2021-01-26 22:53:19 +0400

    various additions

--- src/main/java/HashTable.java
+++ src/main/java/HashTable.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class HashTable {
     public int size;
     public int step;
@@ -12,24 +15,44 @@ public class HashTable {
 
     public int hashFun(String value) {
         // Хэш-функция, которая возвращает корректный индекс слота
-        return 0;
+        return searchNull();
     }
 
     public int seekSlot(String value) {
-        // Метод поиска индекса пустого слота для значения, или -1
-        return -1;
+        // Метод поиска индекса пустого слота для значения
+        // Возвращает номер индекса или -1 при неудаче
+        return searchNull();
     }
 
     public int put(String value) {
         // Метод записи значения по индексу от хэш-функции
         // возвращается индекс слота или -1
         // если из-за коллизий элемент не удаётся разместить
+        int index = seekSlot(value);
+        if (index != -1) {
+            slots[index] = value;
+            return index;
+        }
         return -1;
     }
 
     public int find(String value) {
         // Метод проверки наличия указанного значения
         // Возвращает индекс или -1
+        for (int i = 0; i < size; i++) {
+            if (slots[i].equals(value)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public int searchNull() {
+        for (int i = 0; i < size; i++) {
+            if (slots[i] == null) {
+                return i;
+            }
+        }
         return -1;
     }
 }
