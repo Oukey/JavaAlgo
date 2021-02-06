d07baa8e6add92b1b1ccedeec740fce1c955f977
Oukey <oukey311@gmail.com>
2021-02-06 15:17:10 +0400

    Add solution PowerSet

--- src/main/java/PowerSet.java
+++ src/main/java/PowerSet.java
@@ -1,45 +1,81 @@
+import java.util.ArrayList;
+
 public class PowerSet {
+    public ArrayList set;
+    public int count;
 
     public PowerSet() {
-        // реализация хранилища
+        // реализация хранилища`
+        set = new ArrayList();
+//        count = 0;
     }
 
     public int size() {
         // Метод возвращает количество элементов в множестве
-        return 0;
+        return set.size();
+//        return count;
     }
 
     public void put(String value) {
         // Метод добавления уникального нового элемента
+        if (!get(value)) {
+            set.add(value);
+        }
     }
 
     public boolean get(String value) {
         // Метод проверки вхождения в множество
         // возвращает true если value имеется в множестве,
         // иначе false
+//        return set.contains(value);
+        for (Object elem : set) {
+            if (elem.equals(value)) return true;
+        }
         return false;
+    }
+
+    public Object getToIndex(int index) {
+        // Вспомогательный метод возвращает значение элемента по индексу
+        return set.get(index);
     }
 
     public boolean remove(String value) {
         // Метод удаления из множества
         // возвращает true если value удалено
         // иначе false
-        return false;
+        return set.remove(value);
     }
 
     public PowerSet intersection(PowerSet set2) {
         // Метод пересечения текущего множества и set2
-        return null;
+        PowerSet result = new PowerSet();
+        for (Object elem : set) {
+            if (set2.get((String) elem)) {
+                result.put((String) elem);
+            }
+        }
+        return result;
     }
 
     public PowerSet union(PowerSet set2) {
         // Метод объединяет текущее множество и set2
-        return null;
+        PowerSet result = new PowerSet();
+        for (Object elem : set) {
+            result.put((String) elem);
+        }
+        for (int ind = 0; ind < set2.size(); ind++) {
+            result.put((String) set2.getToIndex(ind));
+        }
+        return result;
     }
 
     public PowerSet difference(PowerSet set2) {
         // Метод возвращают разницу текущего множества и set2
-        return null;
+        PowerSet result = new PowerSet();
+        for (Object elem : set) {
+            if (!set2.get((String) elem)) result.put((String) elem);
+        }
+        return result;
     }
 
     public boolean isSubset(PowerSet set2) {
@@ -47,6 +83,17 @@ public class PowerSet {
         // возвращает true, если set2 есть
         // подмножество текущего множества,
         // иначе false
-        return false;
+        for (int ind = 0; ind < set2.size(); ind++) {
+            if (!set.contains(set2.getToIndex(ind))) return false;
+        }
+        return true;
+    }
+
+    public void show() {
+        System.out.print("[");
+        for (Object elem : set) {
+            System.out.print(" " + elem);
+        }
+        System.out.println(" ]");
     }
 }
